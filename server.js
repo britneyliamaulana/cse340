d8d482a46d9ea3d6b27ec57b847f74b33903ae40
britneyliamaulana <maulanathandeka@gmail.com>
2026-09-06 18:34:22 +0200

    commit all  commit 	new file:   nodemon.json 	new file:   package-lock.json 	new file:   package.json 	new file:   public/css/main.css 	new file:   public/images/brightfuture-logo.png 	new file:   public/images/cse340-service-network.png 	new file:   public/images/greenharvest-logo.png 	new file:   public/images/unityserve-logo.png 	new file:   server.js 	new file:   src/views/home.ejs 	new file:   src/views/organizations.ejs 	new file:   src/views/partials/footer.ejs 	new file:   src/views/partials/header.ejs 	new file:   src/views/projects.ejs

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// EJS setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+
+
+// Home page
+/**
+ * Routes
+ */
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
+});
+
+app.get('/organizations', async (req, res) => {
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title });
+});
+
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects', { title });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://127.0.0.1:${PORT}`);
+});
